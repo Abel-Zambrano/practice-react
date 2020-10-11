8d3fd72b2cea528332acec048b67596168a627f6
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-11 16:32:07 -0700

    added Person component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>This is a React App</h1>
+      <Person />
     </div>
   );
 }
