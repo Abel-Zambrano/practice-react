7d238ca7291177de156e58288d6cba189b0df4d5
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-13 06:41:00 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+
+const App = () => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
-      {name: 'Ethan', age: 6},
-      {name: 'Chloe', age: 3}
-    ],
-  }
+            {name: 'Ethan', age: 6},
+            {name: 'Chloe', age: 3}
+          ]
+  });
 
-  switchNameHandler = () => {
-    console.log('Button Clicked!!');
-    
-  }
+  const [ otherState, setOtherState ] = useState({
+    otherState: 'Some value'
+  })
 
-  render(){
+  console.log(personsState, otherState);
+  
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        {name: 'Sir Ethan!!', age: 6},
+        {name: 'Chloe', age: 3}
+      ]
+    })
+  }
+  
     return (
       <div className="App">
         <h1>This is a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler} >Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >Favorite Food: Pizza</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick={switchNameHandler} >Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} >Favorite Food: Pizza</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
       </div>
     ); 
-  }
-}
+    }
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     persons: [
+//       {name: 'Ethan', age: 6},
+//       {name: 'Chloe', age: 3}
+//     ],
+//     otherState: 'Some value'
+//   }
+
+//   switchNameHandler = () => {
+//     // console.log('Button Clicked!!');
+//     this.setState({
+//       persons: [
+//         {name: 'Sir Ethan!!', age: 6},
+//         {name: 'Chloe', age: 3}
+//       ],
+//     })
+//   }
